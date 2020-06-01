6f62b1cea53c68ac1be55c990b58accb51de8f5c
PrimeNoodles <35935438+PrimeNoodles@users.noreply.github.com>
2020-06-01 13:24:13 -0400

    Uploaded the basic files

---
+++ Assign3.js
@@ -0,0 +1,34 @@
+//Selecting the section with an id of container without using querySelector
+document.getElementById("container");
+
+//Selecting the section with an id of container using querySelector
+document.querySelector("container");
+
+//Selecting all the list items with a class of "second"
+document.getElementsByClassName("second");
+
+//Selecting a list item with a class of third, but only the list item inside of the ol tag
+
+
+//Giving the section with an id of container the text "Hello!"
+
+
+//Adding the class main to the div with a class of footer
+
+
+//Removing the class main on the div with a class of footer
+
+
+//Creating a new li element
+
+
+//Giving the li element the text "four"
+
+
+//Appending the li element to the ul element
+
+
+//Looping over all of the lis inside the ol tag and giving them a background color of "green"
+
+
+//Removing the div with a class of footer
